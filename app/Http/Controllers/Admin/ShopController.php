2947fe3440eb941d9e58dfd6f2b9d89149e85bf1
stian <99058760@mydavinci.nl>
2022-02-14 15:24:55 +0100

    update
    extra update

--- app/Http/Controllers/Admin/ShopController.php
+++ app/Http/Controllers/Admin/ShopController.php
@@ -10,19 +10,17 @@ class ShopController extends Controller
 {
     public function index()
     {
-//        $genres = Genre::get();
-        $records = Record::get();           // get all records
-        $result = compact('records');
-//        $records = Record::with('genres')->paginate(12);
+        $records = Record::with('genres')->paginate(12);
         foreach ($records as $record) {
             $record->cover = $record->cover ?? "https://coverartarchive.org/release/{$record->title_mbid}/front-250.jpg";
         }
-//        $genres = Genre::orderBy('name')            // short version of orderBy('name', 'asc')
-//        ->get();
-//        $result = compact('genres', 'records');
+        $genres = Genre::orderBy('name')            // short version of orderBy('name', 'asc')
+        ->get();
+        $result = compact('genres', 'records');
         $JSON = new Json();
         $JSON->dump($result);
-        return view('shop/index', $result);
+        $result = compact('genres', 'records');
+        return view('shop.index', $result);
     }
 
     public function show($id)
--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -3,19 +3,19 @@
 @section('title', 'Shop')
 
 @section('main')
+
     <h1>Shop</h1>
     <form method="get" action="/shop" id="searchForm">
         <div class="row">
             <div class="col-sm-6 mb-2">
-                <label>
-                    <input type="text" class="form-control" name="artist" value="" placeholder="Filter artist or record">
-                </label>
+                <input type="text" class="form-control" name="artist" value="" placeholder="Filter artist or record">
             </div>
             <div class="col-sm-4 mb-2">
-                <label for="genre_id"></label><select class="form-control" name="genre_id" id="genre_id">
+                <select class="form-control" name="genre_id" id="genre_id">
                     <option value="%">All genres</option>
                     @foreach($genres as $genre)
-                        <option value="{{ $genre->id }}">{{ $genre->name }}</option>
+                        <option value="{{ $genre->id }}"
+                            {{ (request()->genre_id ==  $genre->id ? 'selected' : '') }}>{{ $genre->name }}</option>
                     @endforeach
                 </select>
             </div>
@@ -29,8 +29,8 @@
     <div class="row">
         @foreach($records as $record)
         <div class="col-sm-6 col-md-4 col-lg-3 mb-3">
-            <div class="card">
-                <img class="card-img-top" src="{{ $record->cover }}" alt="{{ $record->artist }} - {{ $record->title }}">
+            <div class="card" data-id="{{ $record->id }}">
+                <img class="card-img-top" src="/assets/vinyl.png" data-src="{{ $record->cover }}" alt="{{ $record->artist }} - {{ $record->title }}">
                 <div class="card-body">
                     <h5 class="card-title">{{ $record->artist }}</h5>
                     <p class="card-text">{{ $record->title }}</p>
